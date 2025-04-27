2df4677fb010b00dda6aae359f6e84d12bcefb44
jingwei.song <jeevsong@gmail.com>
2025-04-27 22:25:33 +0800

    fix: 修复 likecounter 写成 linkCounter 的问题

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,9 @@
 import { Hono } from 'hono'
+import LikeCounter from './models/LikeCounter.ts'
 import user from './modules/user'
 import counter from './modules/counter'
-import { DurableObject, DurableObjectState } from 'cloudflare:workers'
 
-export class LikeCounter extends DurableObject {
-  // In-memory state
-  value = 0
-
-  constructor(ctx: DurableObjectState, env: Env) {
-    super(ctx, env)
-
-    // `blockConcurrencyWhile()` ensures no requests are delivered until initialization completes.
-    ctx.blockConcurrencyWhile(async () => {
-      // After initialization, future reads do not need to access storage.
-      this.value = (await ctx.storage.get('value')) || 0
-    })
-  }
-
-  async getCounterValue() {
-    return this.value
-  }
-
-  async increment(amount = 1): Promise<number> {
-    this.value += amount
-    await this.ctx.storage.put('value', this.value)
-    return this.value
-  }
-
-  async decrement(amount = 1): Promise<number> {
-    this.value -= amount
-    await this.ctx.storage.put('value', this.value)
-    return this.value
-  }
-}
+export { LikeCounter }
 
 type Bindings = {
   LIKE_COUNTER: DurableObjectNamespace<LikeCounter>
