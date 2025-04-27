7543949fc9b5ec757355e4999d4d3df26729bebb
jingwei.song <jeevsong@gmail.com>
2025-04-27 22:22:40 +0800

    fix: 在 entry 到处 LikeCounter 类

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
 import { Hono } from 'hono'
-import LikeCounter from './models/LikeCounter'
 import user from './modules/user'
 import counter from './modules/counter'
+import { DurableObject, DurableObjectState } from 'cloudflare:workers'
 
-export { LikeCounter }
+export class LikeCounter extends DurableObject {
+  // In-memory state
+  value = 0
+
+  constructor(ctx: DurableObjectState, env: Env) {
+    super(ctx, env)
+
+    // `blockConcurrencyWhile()` ensures no requests are delivered until initialization completes.
+    ctx.blockConcurrencyWhile(async () => {
+      // After initialization, future reads do not need to access storage.
+      this.value = (await ctx.storage.get('value')) || 0
+    })
+  }
+
+  async getCounterValue() {
+    return this.value
+  }
+
+  async increment(amount = 1): Promise<number> {
+    this.value += amount
+    await this.ctx.storage.put('value', this.value)
+    return this.value
+  }
+
+  async decrement(amount = 1): Promise<number> {
+    this.value -= amount
+    await this.ctx.storage.put('value', this.value)
+    return this.value
+  }
+}
 
 type Bindings = {
   LIKE_COUNTER: DurableObjectNamespace<LikeCounter>
